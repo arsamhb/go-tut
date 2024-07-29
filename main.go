02c8ad07085fbc87c8446951861a9491121a6328
Arsam <arsam.hb@gmail.com>
2024-07-29 13:13:22 +0330

    dup1

--- main.go
+++ main.go
@@ -9,9 +9,11 @@ import (
 func main() {
 	counts := make(map[string]int)
 	input := bufio.NewScanner(os.Stdin)
+
 	for input.Scan() {
 		counts[input.Text()]++
 	}
+
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
