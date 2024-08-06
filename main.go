d04f16f413b47105badcdaf9b569fdfd3878a9d5
Arsam <arsam.hb@gmail.com>
2024-08-06 14:46:06 +0330

    page80

--- main.go
+++ main.go
@@ -2,15 +2,60 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+	"math"
+	"os"
 )
 
+const (
+	width, height = 600, 320
+	cells         = 100
+	xyRange       = 30.0
+	xyScale       = width / 2 / xyRange
+	zScale        = height * 0.4
+	angle         = math.Pi / 6
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle)
+
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	file, err := os.Create("output.svg")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	fmt.Fprintf(file, "<svg xmlns='http://www.w3.org/2000/svg' "+"style='stroke: gray; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+1)
+			fmt.Fprintf(file, "<polygon fill='#0000ff' points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+	fmt.Fprintf(file, "</svg>")
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+func corner(i, j int) (float64, float64) {
+	x := xyRange * (float64(i)/cells - 0.5)
+	y := xyRange * (float64(j)/cells - 0.5)
+
+	z := f(x, y)
+
+	sx := width/2 + (x-y)*cos30*xyScale
+	sy := height/2 + (x+y)*sin30*xyScale - z*zScale
+	return sx, sy
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y)
+	if !math.IsNaN(math.Sin(r)) {
+		return math.Sin(r)
+	}
+	return 0
 }
